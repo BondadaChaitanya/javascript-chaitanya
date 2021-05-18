2403117d33fea6b4a00cda84b134dda187af8ba9
chaitanya <chaitanya2000a@gmail.com>
2021-05-18 13:11:38 +0530

    adding palindrome algorithm

--- stringreverse.js
+++ stringreverse.js
@@ -1,15 +1,5 @@
-//reverse a string
-var number = 123321;
-var n=number;
-var output = 0;
-while (number > 0) {
-    remainder = number % 10;
-    number = Math.floor(number / 10);
-    output = output * 10 + remainder
-}
-if (output === n) {
-    console.log("given number is palindrome");
-}
-else {
-    console.log("given number is not palindrome")
-}
+var string ="abcde";
+string.split('');
+string.split('').reverse();
+string.split('').reverse().join('');
+console.log(string.split('').reverse().join(''));
